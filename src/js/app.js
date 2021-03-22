73db79f97593ed9bd93ef20bc4ef0190e1a7eb40
oscardenas <oscardenas17@gmail.com>
2021-03-22 18:39:14 -0500

    Creando el HTML con los datos del  .json

--- src/js/app.js
+++ src/js/app.js
@@ -15,12 +15,29 @@ async function mostrarServicios(){
 
        //Generar HTML
        servicios.forEach( servicio => {
-           const { id,nombre,precio} = servicio;
-       });
+           const {id, nombre,precio} = servicio;
+      
 
        //DOM scripting
+       const nombreServicio = document.createElement('P');
+       nombreServicio.textContent = nombre;
+       nombreServicio.classList.add('nombre-servicio');
 
+       const precioServicio = document.createElement('P');
+       precioServicio.textContent = `$ ${precio}`;
+       precioServicio.classList.add('precio-servicio');
 
+       //Generar DIV contenedor de servicio
+       const servicioDiv = document.createElement('DIV');
+       servicioDiv.classList.add('servicio');
+
+       //Inyectar precio y nombre al div servicio
+       servicioDiv.appendChild(nombreServicio);
+       servicioDiv.appendChild(precioServicio);
+
+       //Inyectar el div con los servicios al HTML
+       document.querySelector('#servicios').appendChild(servicioDiv);
+        });
     } catch (error) {
         console.log(error);
     }
